de5a4c17d5c2c48153c63dfef6d2d35e45a528f4
ToWeLong <1191430240@qq.com>
2021-02-01 12:06:02 +0800

    fix: time bug

--- service/moguding.go
+++ service/moguding.go
@@ -113,7 +113,7 @@ func (m moGuService) SignIn(token, planID string) bool {
 		log.Fatal("failed to Load secret ")
 	}
 	// I will go off work at 18:00
-	if time.Now().Hour()+8 >= 12 {
+	if time.Now().UTC().Hour()+8 >= 12 {
 		// go off work sign
 		types = "END"
 	}
--- test/time_test.go
+++ test/time_test.go
@@ -21,7 +21,9 @@ func TestRemoteTime(t *testing.T) {
 	types := "START"
 	fmt.Println("local time:")
 	fmt.Println(time.Now().Format("2006/1/2 15:04:05"))
-	if time.Now().Hour()+8 >= 12 {
+	fmt.Println("UTC time:")
+	fmt.Println(time.Now().UTC().Format("2006/1/2 15:04:05"))
+	if time.Now().UTC().Hour()+8 >= 12 {
 		types = "END"
 		fmt.Println("下班打卡啦~")
 	}
